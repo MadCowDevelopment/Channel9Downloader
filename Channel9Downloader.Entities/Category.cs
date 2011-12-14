8b5b50c4af1528f89970ea8b87ca2e8539b73ead
Mad <madcowdevelopment@googlemail.com>
2011-12-14 08:17:28 +0100

    added some comments

--- Channel9Downloader.Entities/Category.cs
+++ Channel9Downloader.Entities/Category.cs
@@ -1,8 +1,18 @@
 ﻿namespace Channel9Downloader.Entities
 {
+    /// <summary>
+    /// This class serves as base class for different video categories.
+    /// </summary>
     public class Category
     {
+        /// <summary>
+        /// Gets or sets the title of the category.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the relative path for the category.
+        /// </summary>
         public string RelativePath { get; set; }
     }
 }
